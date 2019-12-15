e045764dfb74a1c9ea089e63e841a289dd3a4d6b
Haroun Ach <harounacho21@gamil.com>
2019-12-15 14:42:12 +0100

    Get and query locations in database

--- app/src/main/java/com/harounach/roote/rider/RiderMapsActivity.kt
+++ app/src/main/java/com/harounach/roote/rider/RiderMapsActivity.kt
@@ -3,8 +3,8 @@ package com.harounach.roote.rider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.firebase.geofire.GeoFire
-import com.firebase.geofire.GeoLocation
+import com.firebase.geofire.*
+
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -24,6 +24,7 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityRiderMapsBinding
     private lateinit var map: GoogleMap
+    private lateinit var geoQuery: GeoQuery
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,7 +56,96 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+        // Hand send another location button click
+        binding.sendAnotherLocation.setOnClickListener {
+            geoFire.setLocation(
+                "haroun-another-hq",
+                GeoLocation(35.195015, 4.918146)
+            ) { key, error ->
+                if (error != null) {
+                    Timber.d("There was an error saving the location to GeoFire: $error")
+                } else {
+                    Timber.d("Location saved on server successfully!")
+                }
+            }
+        }
 
+        // Handle get location button click
+        binding.getLocation.setOnClickListener {
+            geoFire.getLocation(
+                "haroun-hq",
+                object : LocationCallback {
+                    override fun onLocationResult(key: String?, location: GeoLocation?) {
+
+                        if (location != null) {
+                            Timber.d(
+                                String.format(
+                                    "The location for key %s is [%f,%f]",
+                                    key,
+                                    location.latitude,
+                                    location.longitude)
+                            )
+                        } else {
+                            Timber.d("No location found for key: $key")
+                        }
+
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError?) {
+                        Timber.d(String.format("There was an error getting the GeoFire location: $databaseError"))
+                    }
+
+                }
+                )
+        }
+        // Handle query locations button click
+        binding.queryLocations.setOnClickListener {
+            geoQuery = geoFire.queryAtLocation(
+                GeoLocation(35.195017, 4.918148),
+                0.6
+            )
+
+            geoQuery.addGeoQueryEventListener(object: GeoQueryEventListener {
+                override fun onGeoQueryReady() {
+                    Timber.d("All initial data has been loaded and events have been fired!")
+                }
+
+                override fun onKeyEntered(key: String?, location: GeoLocation?) {
+                    location?.let {
+                        Timber.d(
+                            String.format(
+                                "Key %s entered the search area at [%f,%f]",
+                                key,
+                                it.latitude,
+                                it.longitude)
+                        )
+                    }
+                }
+
+                override fun onKeyMoved(key: String?, location: GeoLocation?) {
+                    location?.let {
+                        Timber.d(
+                            String.format(
+                                "Key %s moved within the search area to [%f,%f]",
+                                key,
+                                it.latitude,
+                                it.longitude)
+                        )
+                    }
+                }
+
+                override fun onKeyExited(key: String?) {
+                    Timber.d(String.format("Key %s is no longer in the search area", key))
+                }
+
+                override fun onGeoQueryError(error: DatabaseError?) {
+                    Timber.e("There was an error with this query: $error")
+                }
+
+            }
+            )
+
+        }
 
     }
 
@@ -75,5 +165,10 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        geoQuery?.removeAllListeners()
     }
 }
