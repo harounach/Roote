4ef5488830b469366d50a4f6c3da98b7884d6f2f
Haroun Ach <harounacho21@gamil.com>
2019-12-16 16:23:22 +0100

    Save my location to database

--- app/src/main/java/com/harounach/roote/rider/RiderMapsActivity.kt
+++ app/src/main/java/com/harounach/roote/rider/RiderMapsActivity.kt
@@ -1,21 +1,35 @@
 package com.harounach.roote.rider
 
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.firebase.geofire.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-
+import com.google.android.material.snackbar.Snackbar.LENGTH_INDEFINITE
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.harounach.roote.BuildConfig.APPLICATION_ID
 import com.harounach.roote.R
 import com.harounach.roote.databinding.ActivityRiderMapsBinding
 import timber.log.Timber
@@ -25,6 +39,9 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityRiderMapsBinding
     private lateinit var map: GoogleMap
     private lateinit var geoQuery: GeoQuery
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var geoFire: GeoFire
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +55,10 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val database = Firebase.database
         val ref: DatabaseReference = database.getReference("rider/geofire")
 
-        val geoFire = GeoFire(ref)
+        geoFire = GeoFire(ref)
 
+        // Location services client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
 
 
@@ -147,6 +166,16 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
 
+
+        // Send my location to database
+        binding.myLocation.setOnClickListener {
+            if (!checkPermissions()) {
+                requestPermissions()
+            } else {
+                getLastLocation()
+            }
+        }
+
     }
 
     /**
@@ -170,5 +199,116 @@ class RiderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onDestroy() {
         super.onDestroy()
         geoQuery?.removeAllListeners()
+    }
+
+
+    private fun getLastLocation() {
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener { taskLocation ->
+            if (taskLocation.isSuccessful && taskLocation.result != null) {
+                val location = taskLocation.result
+                location?.let {
+                    geoFire.setLocation(
+                        "myLocation",
+                        GeoLocation(it.latitude, it.longitude)
+                    ) { key, error ->
+                        if (error != null) {
+                            Timber.d("There was an error saving the location to GeoFire: $error")
+                        } else {
+                            Timber.d("Location saved on server successfully!")
+                        }
+                    }
+                }
+
+
+            } else {
+                Timber.w("getLastLocation:${taskLocation.exception}")
+                showSnackBar(R.string.no_location_detected)
+            }
+        }
+    }
+
+
+    private fun showSnackBar(
+        snackStrId: Int,
+        actionStrId: Int = 0,
+        listener: View.OnClickListener? = null
+    ) {
+        val snackbar = Snackbar.make(
+            findViewById<ViewGroup>(android.R.id.content),
+            getString(snackStrId), LENGTH_INDEFINITE)
+        if (actionStrId != 0 && listener != null) {
+            snackbar.setAction(getString(actionStrId), listener)
+        }
+        snackbar.show()
+    }
+
+    private fun checkPermissions() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            ACCESS_FINE_LOCATION
+        ) == PERMISSION_GRANTED
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(ACCESS_FINE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    private fun requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION)) {
+            Timber.i("Displaying permission rationale to provide additional context.")
+            showSnackBar(
+                R.string.permission_rationale,
+                android.R.string.ok,
+                View.OnClickListener {
+                    // Request permission
+                    startLocationPermissionRequest()
+                }
+                )
+        } else {
+            Timber.i("Requesting permission")
+            startLocationPermissionRequest()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        Timber.i("")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> {
+                    Timber.i("User interaction was cancelled.")
+                }
+                (grantResults[0] == PERMISSION_GRANTED) -> {
+                    getLastLocation()
+                }
+
+                else -> {
+                    showSnackBar(
+                        R.string.permission_denied_explanation,
+                        R.string.settings,
+                        View.OnClickListener {
+                            val intent = Intent().apply {
+                                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                data = Uri.fromParts("package", APPLICATION_ID, null)
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            }
+                            startActivity(intent)
+                        }
+                        )
+                }
+            }
+        }
+
+    }
+
+
+    companion object {
+        const val REQUEST_PERMISSIONS_REQUEST_CODE = 34
     }
 }
