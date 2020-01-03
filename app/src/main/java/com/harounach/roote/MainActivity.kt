213c6fa58230c56eef4039716acc549df55b4053
Haroun Ach <harounacho21@gamil.com>
2020-01-03 23:35:53 +0100

    Design Login Page

--- app/src/main/java/com/harounach/roote/MainActivity.kt
+++ app/src/main/java/com/harounach/roote/MainActivity.kt
@@ -12,36 +12,10 @@ import com.harounach.roote.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var ref: DatabaseReference
-
-
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        // Firebase realtime database
-        val database = Firebase.database
-        ref = database.getReference("message")
-
-        // Write to database
-        binding.sendButton.setOnClickListener {
-            ref.setValue(binding.editText.text.toString())
-        }
-
-        // Read from database
-        ref.addValueEventListener(object :ValueEventListener{
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("MainActivity", error.toException().toString())
-            }
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val message = dataSnapshot.getValue(String::class.java)
-                binding.resultText.text = message
-            }
-
-        })
-
 
     }
 }
