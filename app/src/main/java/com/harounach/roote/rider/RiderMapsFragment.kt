70264d563a4609f5ac61d3acb212c152c0807d26
Haroun Ach <harounacho21@gamil.com>
2020-01-15 21:16:06 +0100

    Add MapView to RiderMapsFragment

--- app/src/main/java/com/harounach/roote/rider/RiderMapsFragment.kt
+++ app/src/main/java/com/harounach/roote/rider/RiderMapsFragment.kt
@@ -9,6 +9,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 
 import com.harounach.roote.R
@@ -20,7 +25,7 @@ import timber.log.Timber
 /**
  * A simple [Fragment] subclass.
  */
-class RiderMapsFragment : Fragment() {
+class RiderMapsFragment : Fragment(), OnMapReadyCallback {
 
     // Data binding object
     private lateinit var binding: FragmentRiderMapsBinding
@@ -28,6 +33,9 @@ class RiderMapsFragment : Fragment() {
     private lateinit var loginViewModel: LoginViewModel
 
     private lateinit var navController: NavController
+
+    // GoogleMap object obtained when the map is ready
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +49,12 @@ class RiderMapsFragment : Fragment() {
 
         // Find navController
         navController = this.findNavController()
+
+        // Prepare MapView for asynchronous map update
+        binding.riderMapView.getMapAsync(this)
+
+        // Forward onCreate method to MapView
+        binding.riderMapView.onCreate(savedInstanceState)
 
         return binding.root
     }
@@ -74,10 +88,59 @@ class RiderMapsFragment : Fragment() {
     }
 
 
+    /**
+     *  Forward all life cycle methods to MapView
+     * */
+
+    override fun onStart() {
+        super.onStart()
+
+        // Forward onStart method to MapView
+        binding.riderMapView.onStart()
+    }
+
     override fun onResume() {
         super.onResume()
         setUpLoginObservers()
+        // Forward onResume to MapView
+        binding.riderMapView.onResume()
     }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Forward onPause method to MapView
+        binding.riderMapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // Forward onStop method to MapView
+        binding.riderMapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Forward onDestroy method to MapView
+        binding.riderMapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Forward onSaveInstanceState method to MapView
+        binding.riderMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        // Forward onLowMemory method to MapView
+        binding.riderMapView.onLowMemory()
+    }
+
 
     /**
      * Set up observation relationships
@@ -100,6 +163,24 @@ class RiderMapsFragment : Fragment() {
         if (navController.currentDestination!!.id == R.id.riderMapsFragment) {
             navController.navigate(action)
         }
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
     }
 }
